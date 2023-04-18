ac013260ca006d805286bb5f385a9423c5953c40
Richard <richard@stromer.org>
2023-04-17 19:32:26 -0700

    use pandas

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import pandas as pd
 from dateutil.parser import parse
 
 now = parse("Sat Oct 11 17:13:46 UTC 2003")
 print(f"The date is {now.date()}.")
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+print(df)
